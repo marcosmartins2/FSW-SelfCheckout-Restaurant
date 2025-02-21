04fd185aab7ade0100388388551028f5fd01d58d
Marcos Martins <164579513+marcosmartins2@users.noreply.github.com>
2025-02-21 16:02:34 -0300

    add ProductHeader component to enhance product page layout and navigation

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -1,9 +1,9 @@
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import Image from "next/image";
+
 import { notFound } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
 import { db } from "@/lib/prisma";
+
+import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
 
@@ -20,27 +20,7 @@ const ProductPage = async ({params}:ProductPageProps) => {
     return ( <>
     
     
-   <div className="relative w-full h-[300px]">
-    <Image src={product.imageUrl} alt={product.name} fill className="object-contain" />
-    {slug}
-    <div className="relative h-[250px] w-full">
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute left-4 top-4 z-50 rounded-full"
-        
-      >
-        <ChevronLeftIcon />
-      </Button>
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute right-4 top-4 z-50 rounded-full"
-      >
-        <ScrollTextIcon />
-      </Button>
-    </div>
-   </div>
+   <ProductHeader product={product} />
     
     </> );
 }
